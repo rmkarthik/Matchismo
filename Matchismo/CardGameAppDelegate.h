4600695d9c50f9f06a9d34b9a0dc35e9ebc2ca32
Karthik <karthik@Mobilitys-MacBook-Pro-2.local>
2014-02-03 15:33:40 +0530

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Karthik on 2/3/14.
+//  Copyright (c) 2014 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Karthik on 2/3/14.
+//  Copyright (c) 2014 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
